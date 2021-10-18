e7e4111d52a1173994b3a0eb7311de92e478cf2d
chayayot123 <chayayot123@hotmail.com>
2021-10-18 15:34:15 +0700

    Refactor code in main.py and gamelib.py

--- recipe/main.py
+++ recipe/main.py
@@ -1,26 +1,19 @@
 from recipe import Recipe
 
+def make_recipe(name, chocolate=0, coffee=0, milk=0, sugar=0, price=0.0):
+    recipe = Recipe(name)
+    recipe.chocolate = chocolate
+    recipe.coffee = coffee
+    recipe.milk = milk
+    recipe.sugar = sugar
+    recipe.price = price
+
 if __name__ == '__main__':
-    recipe1 = Recipe("Coffee with sugar")
-    recipe1.coffee = 4
-    recipe1.sugar = 2
-    recipe1.milk = 0
-    recipe1.price = 30.0
+    recipe1 = make_recipe("Coffee with sugar", 4, 2, 1, 40.0)
     print(recipe1)
 
-    recipe2 = Recipe("Latte")
-    recipe2.coffee = 3
-    recipe2.sugar = 2
-    recipe2.milk = 1
-    recipe2.price = 40.0
+    recipe2 = make_recipe("Latte", 3, 2, 1, 40.0)
     print(recipe2)
 
-
-    recipe3 = Recipe("Hot Chocolate")
-    recipe3.coffee = 0
-    recipe3.chocolate = 3
-    recipe3.sugar = 2
-    recipe3.milk = 4
-    recipe3.price = 30.0
+    recipe3 = make_recipe("Hot Chocolate", 0, 3, 2, 4, 30.0)
     print(recipe3)
-    
